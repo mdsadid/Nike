3fbf77291bee67588688c91e6afc672c0939636c
Sadid Hasan Rakib <sadid.hasan14@gmail.com>
2024-03-09 21:35:52 +0600

    make markup for hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,34 @@
+import Button from '../components/Button'
+import { arrowRight } from '../assets/icons'
+import { statistics } from '../constants'
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+      <div className="relative xl:w-2/5 w-full flex flex-col justify-center items-start max-xl:padding-x pt-28">
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br/>
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort, and innovation for your active life.
+        </p>
+        <Button label="Shop Now" iconURL={arrowRight}/>
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          {
+            statistics.map(stat => (
+              <div key={stat.label}>
+                <p>{stat.value}</p>
+                <p>{stat.label}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </section>
   )
 }
 
